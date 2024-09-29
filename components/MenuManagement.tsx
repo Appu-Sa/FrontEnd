cb0c361427c302de8a2624db4296ad37ec5a23e6
SANJAY\appus <saisanjay2605@gmail.com>
2024-09-29 11:27:08 +0530

    deploy

--- components/MenuManagement.tsx
+++ components/MenuManagement.tsx
@@ -63,7 +63,7 @@ const Sidebar: React.FC<SidebarProps> = ({
 
   return (
     <div
-      className={`bg-[#101828] text-white h-[750px] fixed rounded-xl left-0 top-0 w-64 transform m-0 md:m-5  ${
+      className={`bg-[#101828] text-white h-[700px] fixed rounded-xl left-0 top-0 w-64 transform m-0 md:m-5  ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out z-30 md:translate-x-0 overflow-y-auto`}
     >
